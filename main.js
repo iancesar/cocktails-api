debc3c406bcadeb9730149f9f0d1a9539686e1b0
Ian <ian@ian-Vostro-3480>
2020-03-18 15:40:36 -0300

    Update main.js, package.json, and yarn.lock

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import express from "express";
+import bodyParser from 'body-parser'
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => res.json({ ok: true }));
 
-app.listen(8080, () => console.log("ESM em com @std/esm"));
+app.listen(8080);

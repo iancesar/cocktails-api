c182158f3e370ee55f0dda8eecd0ff76c9298331
Ian <ian@ian-Vostro-3480>
2020-03-18 16:14:02 -0300

    Update main.js and package.json

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from 'body-parser'
 
+console.log('ENV!!!!!', process.env.NOME);
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
